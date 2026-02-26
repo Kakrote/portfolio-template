2b3a05b40ce1c3e50aef0e8dd2e12f41d6735e69
Kakrote <anshul.pundir111@gmail.com>
2026-02-27 00:08:31 +0530

    added the color changing animation on the buttons

--- components/ui/buttons/index.tsx
+++ components/ui/buttons/index.tsx
@@ -6,8 +6,9 @@ type buttonProps={
 
 export default function Button ({color="#6bc533", cta="Contact us",onClick }:buttonProps){
     return (
-        <div className="p-3 px-5 rounded-lg cursor-pointer" style={{ backgroundColor: color }} onClick={onClick}>
-            <span className="text-white font-bold">{cta}</span>
+        <div className="relative overflow-hidden p-3 px-5 rounded-lg cursor-pointer group" style={{ backgroundColor: color }} onClick={onClick}>
+            <span className="relative z-10 text-white font-bold">{cta}</span>
+            <span className="absolute inset-0 bg-[#496be6] -translate-x-full translate-y-full group-hover:translate-x-0 group-hover:translate-y-0 rounded-lg transition-transform duration-800 ease-in-out"></span>
         </div>
     )
 }
