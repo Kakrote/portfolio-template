958964ee663bcc5f9caa419ea117d91a5204bab2
Kakrote <anshul.pundir111@gmail.com>
2026-02-26 17:39:15 +0530

    about section on working

--- components/sections/about/index.tsx
+++ components/sections/about/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 type About = {
     name: string
     describtion: string
@@ -24,39 +26,27 @@ type AboutData = {
 
 export default function About({ data }: { data: AboutData }) {
     return (
-        <section className=" bg-white h-[80vh]  justify-center items-center py-50 ">
-            <main className="container mx-auto grid grid-cols-3 gap-3">
-
-
-                {/* right section */}
-                <div className=" ">
+        <section className=" bg-white h-[80vh]">
+            <main className="container mx-auto border h-full grid grid-cols-3">
+                {/* right section  */}
+                <div>
                     {/* about */}
                     <div>
-                        <h2 className="text-xl">About</h2>
-                        <p>
-                            <span className="font-bold">Hi, I'm{data.about.name} - </span>
-                            {data.about.describtion}
-                        </p>
+                        <h2>About</h2>
+                        <p><span className="font-bold">Hi, I'm{data.about.name} - </span> {data.about.describtion}</p>
                     </div>
-                    {/* experience */}
-                    <div>
-                        <h2 className="text-xl">Experience: {data.experience.time} </h2>
+
+                    {/* experiences */}
+                    <div className="max-w-xs">
+                        <h2>Experinces: {data.experience.time}</h2>
                         <p>{data.experience.describtion}</p>
-                        <span>Skills: </span>
-                        <ul>
-                            {data.experience.skills.map((skill, index) => (
-                                <li key={index}>{skill}</li>
-                            ))}
-                        </ul>
                     </div>
                 </div>
-
-                {/* central image  */}
+                {/* central section */}
                 <div>
 
                 </div>
-
-                {/* left section with kpis */}
+                {/* left kpis section */}
                 <div>
 
                 </div>
